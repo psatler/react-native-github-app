3feaac2c6801385cad29c62b28d4619945f265c8
psatler <pablosatler@gmail.com>
2019-07-09 20:31:02 -0300

    chore: config reactotron

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import Reactotron from 'reactotron-react-native';
+
+import './config/ReactotronConfig';
+
+Reactotron.warn('hello rendering world');
+
 const App = () => {
   return (
     <View>
-      <Text>Hfdsfellofdsf World!</Text>
+      <Text>Hfdsff wLRD!</Text>
     </View>
   );
 };
