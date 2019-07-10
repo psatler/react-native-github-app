6b619bac86281668981af14d3b6b4396dc2607de
psatler <pablosatler@gmail.com>
2019-07-10 00:17:41 -0300

    feat: add style to User screen and show starred repos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
 import Reactotron from 'reactotron-react-native';
 import api from '../../services/api';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 // Reactotron.log(navigation.getParam('user'));
 export default class User extends Component {
@@ -29,13 +41,37 @@ export default class User extends Component {
       stars: response.data,
     });
   }
+  // Stars is a FlatList
+  // TODO. onEndReached, fetch more starred repos from API
+  // TODO add a loading status
+  // when clicking on a link to repo, open a webview
 
   render() {
     const { stars } = this.state;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
     return (
-      <View>
-        <Text>User</Text>
-      </View>
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name} </Name>
+          <Bio> {user.bio} </Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name} </Title>
+                <Author>{item.owner.login} </Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
     );
   }
 }
