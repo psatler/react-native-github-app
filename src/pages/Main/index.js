510b5e2eac47735b5339affc8bc941e712ffe9cd
psatler <pablosatler@gmail.com>
2019-07-09 22:00:45 -0300

    chore: install react-native-vector-icons and link it
    - also styled the Main component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 const Main = () => {
   return (
     <Container>
-      <Text>Main</Text>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 };
