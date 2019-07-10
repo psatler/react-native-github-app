9829eae701843a1841fc4eac40a2eef83da9b195
psatler <pablosatler@gmail.com>
2019-07-09 21:27:19 -0300

    chore: install styled-components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { Container } from './styles';
+
 const Main = () => {
   return (
-    <View>
+    <Container>
       <Text>Main</Text>
-    </View>
+    </Container>
   );
 };
 
