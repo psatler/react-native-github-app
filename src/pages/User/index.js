23ef881ef574f4b824b68a3df7e6dd677c82e33e
psatler <pablosatler@gmail.com>
2019-07-09 23:49:15 -0300

    feat: send request to github api to list all starred repos from a given user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
-
 import Reactotron from 'reactotron-react-native';
+import api from '../../services/api';
 
-const User = ({ navigation }) => {
-  Reactotron.log(navigation.getParam('user'));
+// Reactotron.log(navigation.getParam('user'));
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-  return (
-    <View>
-      <Text>User</Text>
-    </View>
-  );
-};
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
 
-export default User;
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({
+      stars: response.data,
+    });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return (
+      <View>
+        <Text>User</Text>
+      </View>
+    );
+  }
+}
