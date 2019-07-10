d3b636d76f2d82ac825044d40195af245e5248b5
psatler <pablosatler@gmail.com>
2019-07-09 21:18:48 -0300

    chore: use statusbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 import Reactotron from 'reactotron-react-native';
@@ -7,7 +7,12 @@ import Reactotron from 'reactotron-react-native';
 import Routes from './routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <Fragment>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </Fragment>
+  );
 };
 
 const styles = StyleSheet.create({});
